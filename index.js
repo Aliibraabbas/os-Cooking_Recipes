907e4330ae9e69b7045da59daae89987de029b9f
Ali ABBAS <ali.ibra.abbas@gmail.com>
2024-07-09 14:20:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-import average from '../src/average.js';
-import median from '../src/median.js';
 import searchRecipes from '../src/searchRecipes.js';
-
-console.log(average([1, 2, 3])); 
-console.log(median([1, 2, 3, 3, 3]));
 
 export {
     searchRecipes,
